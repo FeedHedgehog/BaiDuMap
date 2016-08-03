5d27c4be26aa343744a041f485d022f4fb07d26a
FeedHedgehog <1424870395@qq.com>
2016-08-03 11:07:51 +0800

    解决乱码问题，解决办法 将编码保存为UTF-8保存并覆盖原来的文件 增加快速生成的地图Html页面 并将html和js代码拆分开来

---
+++ sample/formWithJs/JSFrmApp/JS.Model/Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace JS.Model
+{
+    /// <summary>
+    /// 坐标点类
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    public class Point
+    {
+        [DataMember]
+        public string ID { get; set; }
+        [DataMember]
+        public string Longitude { get; set; }
+        [DataMember]
+        public string Latitude { get; set; }
+    }
+}
