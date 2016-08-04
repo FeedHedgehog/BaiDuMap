3842486744d6b6d37b5eddeffa8669cb2b583e72
FeedHedgehog <1424870395@qq.com>
2016-08-04 15:25:38 +0800

    合并公用的坐标点集合，本地html加载java 的restful服务数据，存在跨域问题，所有这边暂时暂停此种解决方案，采用Form下的webBrowser来 传递数据进来

--- sample/formWithJs/JSFrmApp/bin/Debug/html/js/map-action.js
+++ sample/formWithJs/JSFrmApp/bin/Debug/html/js/map-action.js
@@ -1,8 +1,31 @@
 ﻿    'use strict';
 
+	//login
+	function loginServer(){
+		$.post("http://192.168.60.173:8080/cbs/login", 
+		{ 
+			j_username:"admin",
+			j_password:"111111",
+			remember_me:"on",
+			j_type:"mobile"
+		}, 
+			function(data,status){ 
+			alert("Login 数据: \n" + data + "\n状态: " + status); 
+		}); 
+	}
+	
+	//get data from server
+	function getDataFromServer(){
+		$.get("http://httpbin.org/",function(data,status){
+			alert("数据: " + data + "\n状态: " + status);
+		  });
+	}
+	
+	
+	
     //create  and initialize the baidu map.
     function initMap(){
-		//create the baidu map
+	  //create the baidu map
       createMap();
 	  //set map's events
       setMapEvent();
@@ -26,9 +49,19 @@
       map.enableKeyboard();
       map.enableDragging();
       map.enableDoubleClickZoom()
+	  map.addEventListener("click",function(e){
+		  if(e.overlay){
+			selectAction();
+		}
+		else{
+			unselectAction();
+		}
+	  });
       //custom events
 	  addZoomEndHandler(15);	  
     }
+	
+	
 	
 	//add ClickEvent for tartget,and after that,open the InfoWindow which passed on
     function addClickHandler(target,window){
@@ -49,7 +82,7 @@
 				//addMapOverlay();
 				addCustomPolyline(true);
 				
-				addMarkerInfoWindow("imgDemo",sContent,inputzoom);				
+				//addMarkerInfoWindow("imgDemo",sContent,inputzoom);				
 			}	
 			else{
 				addCustomPolyline(false);
@@ -86,7 +119,7 @@
 			
 			map.clearOverlays();			
 			map.addOverlay(polyline); 	
-			polyline.addEventListener("click",getAttr);
+			polyline.addEventListener("click",clickPolylineHandler);
 			polyline.show();
 		}
 		else{
@@ -96,8 +129,9 @@
 		//alert(isShow);
 	}
 	
-	function getAttr(){
-			
+	//点击折线的时候改变颜色和透明度
+	function clickPolylineHandler(e){
+		
 	}
 	
 	//add label to Polyline
@@ -125,6 +159,27 @@
 	"<img style='float:right;margin:4px' id='imgDemo' src='http://app.baidu.com/map/images/tiananmen.jpg' width='139' height='104' title='天安门'/>" + 
 	"<p style='margin:0;line-height:1.5;font-size:13px;text-indent:2em'>六盘水是...</p>" + 
 	"</div>";
+	
+	var polylines = [
+	new BMap.Polyline([
+		  new BMap.Point(104.853864,26.598749),
+          new BMap.Point(104.848474,26.593128),
+          new BMap.Point(104.861553,26.58854),
+          new BMap.Point(104.864931,26.587829),
+          new BMap.Point(104.868165,26.58686),
+          new BMap.Point(104.869458,26.592223),
+          new BMap.Point(104.86989,26.593322),
+          new BMap.Point(104.860619,26.595841),
+          new BMap.Point(104.86141,26.597844),
+          new BMap.Point(104.858607,26.598943),
+          new BMap.Point(104.856738,26.598038),
+          new BMap.Point(104.855373,26.598167),
+          new BMap.Point(104.853792,26.598878),
+          new BMap.Point(104.853792,26.598878),
+          new BMap.Point(104.853792,26.598878),
+          new BMap.Point(104.853792,26.598878)
+		], {strokeColor:"blue", strokeWeight:20, strokeOpacity:0.5})
+	];
 		
 	function addMarkerInfoWindow(imgid,content,zoom){	
 		var pt = new BMap.Point(104.857483,26.597535);	
@@ -221,41 +276,41 @@
 	
 	
 	// 改变覆盖物状态  
-function change(){  
-	var polylines = [
-	new BMap.Polyline([
-		  new BMap.Point(104.853864,26.598749),
-          new BMap.Point(104.848474,26.593128),
-          new BMap.Point(104.861553,26.58854),
-          new BMap.Point(104.864931,26.587829),
-          new BMap.Point(104.868165,26.58686),
-          new BMap.Point(104.869458,26.592223),
-          new BMap.Point(104.86989,26.593322),
-          new BMap.Point(104.860619,26.595841),
-          new BMap.Point(104.86141,26.597844),
-          new BMap.Point(104.858607,26.598943),
-          new BMap.Point(104.856738,26.598038),
-          new BMap.Point(104.855373,26.598167),
-          new BMap.Point(104.853792,26.598878),
-          new BMap.Point(104.853792,26.598878),
-          new BMap.Point(104.853792,26.598878),
-          new BMap.Point(104.853792,26.598878)
-		], {strokeColor:"blue", strokeWeight:20, strokeOpacity:0.5})
-	];
+function selectAction(){  	
 	for (var i=0; i<polylines.length; i++)
 	{
 		var style = polylines[i].getStrokeStyle();
 		if(style == "solid"){
 			polylines[i].setStrokeColor("green");  
 			polylines[i].setStrokeOpacity(1);
-			alert("solied,and opacity is"+polylines[i].getStrokeOpacity());
+			map.clearOverlays();
+			map.addOverlay(polylines[i]);
+			//alert("solied,and opacity is"+polylines[i].getStrokeOpacity());
 		}
 		else if(style == "dashed"){
-			alert("dashed");
+			//alert("dashed");
 			polylines[i].setStrokeColor("yellow"); 
 		}
 	}
 }  
+
+function unselectAction(){	
+	for (var i=0; i<polylines.length; i++)
+	{
+		var style = polylines[i].getStrokeStyle();
+		if(style == "solid"){
+			polylines[i].setStrokeColor("blue");  
+			polylines[i].setStrokeOpacity(0.5);
+			map.clearOverlays();
+			map.addOverlay(polylines[i]);
+			//alert("solied,and opacity is"+polylines[i].getStrokeOpacity());
+		}
+		else if(style == "dashed"){
+			//alert("dashed");
+			polylines[i].setStrokeColor("yellow"); 
+		}
+	}
+}
   
 
 	
